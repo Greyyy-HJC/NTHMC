960853be7e0ab384831d6d321a55dee41ff9a492
Greyyy-HJC <hejinchen17@gmail.com>
2026-05-01 17:00:33 +0000

    update

--- 2du2/evaluation/base/run.sh
+++ 2du2/evaluation/base/run.sh
@@ -1,1 +1,1 @@
-python compare_fthmc.py --lattice_size 8 --n_configs 128 --beta 2.0 --train_beta 2.0 --n_thermalization 10 --n_steps 4 --ft_step_size 0.1 --max_lag 20 --rand_seed 1029 --model_tag 'base' --save_tag 'base_train_b2.0_L8_1029' --device 'cuda' --no_tune_step_size
+python compare_fthmc.py --lattice_size 8 --n_configs 128 --beta 2.0 --train_beta 2.0 --n_thermalization 10 --n_steps 10 --ft_step_size 0.1 --max_lag 20 --rand_seed 1029 --model_tag 'base' --save_tag 'base_train_b2.0_L8_1029' --device 'cuda' --no_tune_step_size
